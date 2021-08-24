ac9c6f6f05d0f9788b59a02aaa87b2f4637c84ed
Gospel Chukwu <brondchux@gmail.com>
2021-08-24 19:09:57 -0400

    Defined all the required sequelize model columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,26 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+	{
+		// define columns
+		product_id: {
+			type: DataTypes.INTEGER,
+		},
+		tag_id: {
+			type: DataTypes.INTEGER,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "product_tag",
+	}
 );
 
 module.exports = ProductTag;
